4d56911b86db9703f289f357dcf1c4580be3a8cd
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2021-11-17 22:15:22 -0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginForm from './src/components/LoginForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello perros !</Text>
+      <Text>Hola gatos tambien uwu</Text>
+      <LoginForm/>
     </View>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { TextInput, Button, Text, View } from "react-native";
+
+export default function LoginForm() {
+  return (
+    <View>
+      <Text>Componente LoginForm</Text>
+      <Text>perreo desde componente</Text>
+    </View>
+  );
+}
